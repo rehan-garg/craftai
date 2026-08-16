fed72071d129719dba03e3a223d15d2d1b0f75ef
Rehan Garg <rehangrg2007@gmail.com>
2026-08-16 22:03:59 +0530

    fix production cors

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,11 @@ app = FastAPI(title="CraftAI Backend", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://craftai-s98r.onrender.com/"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://craftai-s98r.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
